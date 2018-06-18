42ed941c01cc2e54528d6dde8313a442e9e9dfdc
Axel Loupias <axelloupias@gmail.com>
2018-06-18 15:04:35 +0200

    Creación del HeaderNavWalker.

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require get_template_directory().'/components/header-menu-walker.php';
+
 add_action('after_setup_theme','bval_setup');
 function bval_setup(){
   add_image_size( 'entradas', 750, 490, true );
@@ -8,7 +10,7 @@ function bval_setup(){
   //Allow post thumbnails
   add_theme_support( 'post-thumbnails' );
   //Allow customize the logo whith a size
-  add_theme_support( 'custom-logo',array('height'=>100,'width'=>500));
+  add_theme_support( 'custom-logo',array('height'=>80,'width'=>400));
 }
 
 add_action('wp_enqueue_scripts', 'bval_styles');
@@ -24,11 +26,21 @@ function bval_styles(){
 add_action( 'wp_enqueue_scripts','bval_scripts');
 function bval_scripts(){
     wp_register_script('bootrstrapJS', get_template_directory_uri().'/js/bootstrap.js',  array('jquery'), '4.1', true );
-    wp_register_script('scripts', get_template_directory_uri().'/js/scripts.js',  array('jquery','bootstrap'), '1.0', true );
+    wp_register_script('scripts', get_template_directory_uri().'/js/scripts.js',  array('jquery','bootrstrapJS'), '1.0', true );
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootrstrapJS');
     wp_enqueue_script('scripts');
 }
 
+
+add_action('init','bval_menus');
+function bval_menus(){
+  register_nav_menus(array(
+    'header-menu'=> __('Header Menu', 'al_blogViajes'),
+    'social-menu'=> __('Social Menu', 'al_blogViajes'),
+    'front-menu'=> __('Página Inicio Menu', 'al_blogViajes'),
+  ));
+
+}
 
  ?>
--- header.php
+++ header.php
@@ -8,3 +8,31 @@
   <?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
+  <header class="site-header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+      <div class="container">
+        <a class="navbar-brand" href="<?php echo esc_url(home_url( '/' )); ?>">
+          <?php
+          $custom_logo_id = get_theme_mod( 'custom_logo' );
+          $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+          ?>
+          <img src="<?php echo $image[0]; ?>" alt="" class="img-fluid">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuPrincipal" aria-controls="menuPrincipal" aria-expanded="false" aria-label="Boton de expandir">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menuPrincipal">
+          <?php
+          wp_nav_menu( array(
+            'theme_location'=>'header-menu',
+            'container_class' => 'navbar-nav',
+            'items_wrap'=> '%3$s',//Para quitar el ul
+            'walker'=> new HeaderMenuWalker()
+          ) );
+          ?>
+
+        </div>
+      </div>
+    </nav>
+  </header>
